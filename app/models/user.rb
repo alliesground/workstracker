ff04ef8e816f2cba2657f79df3983f1df46cbc34
Sameer Subba <sameer.libang@gmail.com>
2017-09-04 14:35:25 +1000

    generate custom devise views for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
   rolify
+  after_create :assign_role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
+  def assign_role
+    self.add_role "freelancer"
+  end
 end
