d0d9d2c3534ac98f4d75c8a4ccaa3d11ff07895b
Sameer Subba <sameer.libang@gmail.com>
2017-09-17 16:59:36 +1000

    create project ui and shared views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  get 'ui(/:action)', controller: 'ui'
   root to: "home#index"
   resources :users, only: :show
   resources :projects, only: [:new, :create]
