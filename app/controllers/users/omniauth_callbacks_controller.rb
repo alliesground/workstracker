01ef87a02846fe12aa1ce60644344e585a50c46d
Sameer Subba <sameer.libang@gmail.com>
2017-11-10 17:14:33 +1100

    post implementation of choosing roles

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,13 +3,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      if @user.roles.blank?
-        sign_in @user
-        redirect_to choose_role_path and return
-      end
-
-      sign_in_and_redirect @user, :event => :authentication
+      sign_in @user
       set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+
+      redirect_to choose_role_path and return if @user.roles.blank?
+      redirect_to @user
     else
       session["devise.github_data"] = request.env["omniauth.auth"]
       redirect_to root_path, flash: { erros: 'Authentication failed' }
