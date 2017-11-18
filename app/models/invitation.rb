6de49fcd53e4bee189388588e78abab08d6258f7
Sameer Subba <sameer.libang@gmail.com>
2017-11-18 16:29:01 +1100

    post invitation email validation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,7 +3,11 @@ class Invitation < ApplicationRecord
 
   belongs_to :inviter, class_name: 'User'
 
+  VALID_EMAIL_REGEXP = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
   validates_presence_of :recipient_name,
                         :recipient_email,
                         :message
+  validates :recipient_email, length: { maximum: 255 },
+                             format: { with: VALID_EMAIL_REGEXP },
+                             uniqueness: { case_sensitive: false }
 end
