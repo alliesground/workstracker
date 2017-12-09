a78f5f7230b01c615a75fb81e6a0c10e65f14d9c
Sameer Subba <sameer.libang@gmail.com>
2017-12-09 14:42:22 +1100

    normal login with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { 
+    :omniauth_callbacks => "users/omniauth_callbacks",
+    :registrations => 'users/registrations'
+  }
 
   get 'ui(/:action)', controller: 'ui'
   root to: "home#index"
