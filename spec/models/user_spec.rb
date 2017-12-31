b3da479d402a8c3646fb7a7bde97ce5cb3910478
Sameer Subba <sameer.libang@gmail.com>
2018-01-01 00:31:26 +1100

    passing User#roles_scoped_to unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe User do
+  let(:user) { create(:user) }
+
   describe '#has_any_role_scoped_to?' do
     it 'returns true if a user is assigned a role scoped to the given resource' do
-      user = create(:user)
       project = create(:project, user: user)
 
       expect(user.has_any_role_scoped_to?(resource: project)).to be true
@@ -11,6 +12,15 @@ describe User do
   end
 
   describe '#roles_scoped_to' do
-    it 'returns'
+    it 'returns all the roles of a user that is scoped to a resource' do
+      invited_user = create(:user)
+      project = create(:project)
+      invited_user.add_role('client', project)
+      invited_user.add_role('collaborator', project)
+
+      expect(
+        invited_user.roles_scoped_to(resource: project).size
+      ).to eq 2
+    end
   end
 end
