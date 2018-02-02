224b3414e05de8499c807c8a7837da9b897a4b6c
Sameer Subba <sameer.libang@gmail.com>
2018-02-03 01:32:55 +1100

    unit testing stakeholder model

--- app/models/stakeholder.rb
+++ app/models/stakeholder.rb
@@ -8,7 +8,14 @@ class Stakeholder
   end
 
   def self.all_scoped_to(resource:)
-    users = User.joins("INNER JOIN users_roles ON users_roles.user_id = users.id INNER JOIN roles ON roles.id = users_roles.role_id").merge(Role.scoped_to(resource_id: resource.id))
+    users = User.joins(
+      <<~SQL
+        INNER JOIN users_roles
+        ON users_roles.user_id = users.id
+        INNER JOIN roles
+        ON roles.id = users_roles.role_id
+      SQL
+    ).merge(Role.scoped_to(resource_id: resource.id))
 
     users.map {|user| new(user: user)}
   end
--- spec/models/stakeholder_spec.rb
+++ spec/models/stakeholder_spec.rb
@@ -2,13 +2,27 @@ require 'rails_helper'
 
 describe Stakeholder do
   describe '.all_scoped_to' do
-    it 'returns all users having roles scoped to the given resource, wrapped around stakeholder object' do
-      user = create(:user, email: 'test@email.com')
-      resource = create(:project, user: user)
-      stakeholder = Stakeholder.new(user: user)
+    it 'returns all stakeholders having roles scoped to the given resource' do
+      owner = create(:user, email: 'test1@email.com')
+      stakeholder = create(:user, email: 'test2@email.com')
+      resource = create(:project, user: owner)
+      stakeholder.add_role('collaborator', resource)
 
-      stakeholder = Stakeholder.all_scoped_to(resource: resource).first
-      expect(stakeholder.user).to eq user
+      stakeholders = Stakeholder.all_scoped_to(resource: resource)
+
+      expect(stakeholders.size).to eq(2)
+    end
+
+    it 'does not return a stakeholder who does not have roles scoped to the given resource' do
+      user = create(:user, email: 'test1@email.com')
+      resource = create(:project, user: user)
+
+      user2 = create(:user, email: 'test2@email.com')
+      other_resource = create(:project, user: user2)
+      user.add_role('collaborator', other_resource)
+
+      stakeholders = Stakeholder.all_scoped_to(resource: resource)
+      expect(stakeholders.size).not_to eq(2)
     end
   end
 end
