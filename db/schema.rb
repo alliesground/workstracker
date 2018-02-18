65002e7544f1b6bc5e9e4a8172f0e5d39257311b
Sameer Subba <sameer.libang@gmail.com>
2018-02-18 17:03:55 +1100

    removed devise-jwt blacklist table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131234338) do
+ActiveRecord::Schema.define(version: 20180218000706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20180131234338) do
     t.index ["recipient_id"], name: "index_invitations_on_recipient_id"
     t.index ["status"], name: "index_invitations_on_status"
     t.index ["token"], name: "index_invitations_on_token"
+  end
+
+  create_table "jwt_blacklist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_blacklist_on_jti"
   end
 
   create_table "projects", force: :cascade do |t|
