e927f06407c6244b41090c160858a11adc7bc28b
Sameer Subba <sameer.libang@gmail.com>
2018-03-23 15:48:51 +1100

    post add JTIMatcher revocation strategy

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
   has_many :invitations, dependent: :destroy, foreign_key: :inviter_id
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
 
   def has_any_role_scoped_to?(resource:)
     roles.merge(Role.scoped_to(resource_id: resource.id)).exists?
