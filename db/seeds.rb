4a732ceeaf6be72357648038a0f5dd8647d956eb
Sameer Subba <sameer.libang@gmail.com>
2018-10-30 00:27:13 +1100

    Post seeding dummy user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.where(email: ENV['TEST_USER_EMAIL'])
+unless user.exists?
+  puts "Creating test User"
+  User.create!(email: ENV['TEST_USER_EMAIL'], password: ENV['TEST_USER_PASSWORD'], confirmed_at: Time.now)
+else
+  puts "dummy user exists"
+end
