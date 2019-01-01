692c37fcca106204259367b1fcbd44f59cddf3f1
Sameer Subba <sameer.libang@gmail.com>
2019-01-01 19:16:59 +1100

    Post adding default admin membership role on project creation

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,12 +2,4 @@ class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :project
   has_one :membership_role
-
-  after_create :assign_default_membership_role
-
-  private
-
-  def assign_default_membership_role
-    create_membership_role
-  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
+  after_commit :assign_default_membership_role, on: :create
+
   validates_presence_of :title
 
   has_many :memberships
@@ -6,5 +8,11 @@ class Project < ApplicationRecord
 
   def stakeholders
     Stakeholder.all_scoped_to(resource: self)
+  end
+
+  private
+
+  def assign_default_membership_role
+    memberships.last.create_membership_role
   end
 end
