6fbcdf31991e1fe2eb2e8637993862406be04cb3
Sameer Subba <sameer.libang@gmail.com>
2019-06-23 18:22:51 +1000

    adding owner field to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord
   validates_presence_of :title
 
+  belongs_to :owner, class_name: 'User'
+
   has_many :memberships, as: :resource, dependent: :destroy
   has_many :members, through: :memberships, :source => :user
 
