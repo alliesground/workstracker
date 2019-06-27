058a150b28787634d6003e16f483b2ed98b3a4ec
Sameer Subba <sameer.libang@gmail.com>
2019-06-27 13:36:58 +1000

    production config update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,9 +72,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
-    :authentication       => "plain",
+    :domain               => "gmail.com",
+    :user_name            => ENV['GMAIL_USER'],
+    :password             => ENV['GMAIL_PASS'],
+    :authentication       => "login",
     :enable_starttls_auto => true
   }
 
