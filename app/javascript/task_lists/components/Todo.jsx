3531662ac4a6a5b5a2c5b346b65c8f255c6770b0
Sameer Subba <sameer.libang@gmail.com>
2019-08-13 13:55:20 +1000

    react - updating todo

--- app/javascript/task_lists/components/Todo.jsx
+++ app/javascript/task_lists/components/Todo.jsx
@@ -1,16 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Checkbox } from 'semantic-ui-react';
-import { useToggle } from './useToggle';
 import { List } from 'semantic-ui-react';
+import { useEndpoint } from './useEndpoint';
 
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo }) => { 
 
-  const [checked, toggleChecked] = useToggle();
+  const [checked, setChecked] = useState(false);
+  const [checkListUpdated, setCheckListUpdated] = useState(false);
 
-  const handleTaskComplete = (e) => {
-    toggleChecked();
+  const [updatedTodo, updateTodo] = useEndpoint(data => ({
+    url: `todos/${todo.id}`,
+    method: 'PUT',
+    body: JSON.stringify(data),
+    headers: {
+      'Accept': 'application/vnd.api+json',
+      'Content-Type': 'application/vnd.api+json',
+    }
+  }));
+
+  const handleTaskComplete = () => {
+    setCheckListUpdated(true)
+    setChecked(!checked);
   }
+
+  useEffect(() => {
+    if(checkListUpdated) {
+      updateTodo({
+        data: {
+          id: todo.id,
+          type: 'todos',
+          attributes: {
+            completed: checked
+          }
+        }
+      });
+    } else {
+      setChecked(todo.attributes.completed)
+    }
+  },[checked]);
 
   return(
     <List.Item>
@@ -18,6 +46,7 @@ const Todo = ({ todo }) => {
         label= {todo.attributes.title} 
         onChange={handleTaskComplete}
         style={checked ? {textDecoration:"line-through"} : null}
+        checked={checked}
       />
     </List.Item>
   );
