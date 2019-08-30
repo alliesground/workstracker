106f95038f2166ab046cfc83e84b84dad1e72c42
Sameer Subba <sameer.libang@gmail.com>
2019-08-30 19:57:34 +1000

    fix: action cable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Set cable server's URI
-  config.web_socket_server_url = "wss://https://staging-workstracker.herokuapp.com/cable" 
+  config.web_socket_server_url = "wss://staging-workstracker.herokuapp.com/cable" 
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -45,7 +45,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://https://staging-workstracker.herokuapp.com', 'http://https://staging-workstracker.herokuapp.com']
+  config.action_cable.allowed_request_origins = [ 'https://staging-workstracker.herokuapp.com', 'http://staging-workstracker.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
