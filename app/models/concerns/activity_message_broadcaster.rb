be892d51acb15bf2f2beb698eae921f32394f3bd
Sameer Subba <sameer.libang@gmail.com>
2019-09-02 22:47:27 +1000

    real time notification on creating list

--- app/models/concerns/activity_message_broadcaster.rb
+++ app/models/concerns/activity_message_broadcaster.rb
@@ -10,9 +10,27 @@ module ActivityMessageBroadcaster
   def broadcast_activity_message
     ActivitiesChannel.broadcast_to(
       project,
-      activity_message: activity_message,
-      activity_owner_id: activities.last.owner_id 
+      response: json_response,
+      message: activity_message,
+      ownerId: activities.last.owner_id 
     )
+  end
+
+  def json_response
+    return unless class_exists?("#{self.class.name}Resource")
+
+    JSONAPI::ResourceSerializer.new(resource_class).serialize_to_hash(ListResource.new(self, nil))
+  end
+
+  def resource_class
+    "#{self.class.name.classify}Resource".constantize
+  end
+
+  def class_exists?(class_name)
+    klass = Module.const_get(class_name)
+    return klass.is_a?(Class)
+  rescue NameError
+    return false
   end
 
   def activity_owner_email
